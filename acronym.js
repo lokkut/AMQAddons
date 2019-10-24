0b27090944d64cee3e6f1ebeec76fe32154bbe34
lokkut <lokkut3@gmail.com>
2019-10-24 19:36:49 +0100

    attempt to speed up the entry box loading time

--- acronym.js
+++ acronym.js
@@ -45,5 +45,94 @@ AmqAwesomeplete.prototype.evaluate = function () {
 		this.acronym = null;
 	}
 
-	_OldAwesomepleteEvaluate.call(this);
+	//_OldAwesomepleteEvaluate.call(this);
+
+	let me = this;
+	let unescapedValue = this.input.value;
+	let value = createAnimeSearchRegexQuery(unescapedValue);
+	let inputList;
+
+	if (this.currentQuery && new RegExp(this.currentQuery, 'i').test(unescapedValue) && this.currentSubList) {
+		inputList = this.currentSubList;
+	} else if (unescapedValue.length > 0) {
+		let letterList = this.letterLists[unescapedValue[0].toLowerCase()];
+		if (letterList) {
+			inputList = letterList;
+		} else {
+			inputList = [];
+		}
+		this.currentSubList = inputList;
+	} else {
+		inputList = this._list;
+		this.currentSubList = null;
+	}
+
+	this.currentQuery = value;
+
+	if (value.length >= this.minChars && inputList.length > 0) {
+		this.searchId++;
+		let currentSearchId = this.searchId;
+		$("#qpAnswerInputLoadingContainer").removeClass("hide");
+		this.index = -1;
+		// Populate list with options that match
+		this.$ul.children('li').remove();
+
+		let suggestions = [];
+		let selectedItems = [];
+
+		let handlePassedSuggestions = function (me) {
+			if (this.sort !== false) {
+				this.suggestions = this.suggestions.sort(this.sort);
+			}
+			this.currentSubList = selectedItems;
+			this.suggestions = this.suggestions.slice(0, this.maxItems);
+
+			for (let i = this.suggestions.length - 1; i >= 0; i--) {
+				let text = this.suggestions[i];
+				me.ul.insertBefore(me.item(text, value, i), me.ul.firstChild);
+			}
+
+			if (this.ul.children.length === 0) {
+
+				this.status.textContent = "No results found";
+
+				this.close({ reason: "nomatches" });
+
+			} else {
+				this.open();
+				this.status.textContent = this.ul.children.length + " results found";
+			}
+			$("#qpAnswerInputLoadingContainer").addClass("hide");
+		}.bind(this);
+
+		let timeoutLoop = function (index, me, handlePassedSuggestions, currentSearchId) {
+			if (currentSearchId !== this.searchId) {
+				return;
+			}
+
+			if (index < inputList.length) {
+				for (let i = index; i < inputList.length && i < index + 1000; i++) {
+					let item = inputList[i];
+					let suggestion = new Suggestion(me.data(item, value));
+					if (me.filter(suggestion, value)) {
+						selectedItems.push(item);
+						suggestions.push(suggestion);
+					}
+				}
+				setTimeout(function () {
+					timeoutLoop(index + 1000, me, handlePassedSuggestions, currentSearchId);
+				}.bind(this), 0);
+			} else {
+				this.suggestions = suggestions;
+				handlePassedSuggestions(me);
+			}
+		}.bind(this);
+
+		timeoutLoop(0, me, handlePassedSuggestions, currentSearchId);
+	}
+	else {
+		this.close({ reason: "nomatches" });
+
+		this.status.textContent = "No results found";
+	}
 }
